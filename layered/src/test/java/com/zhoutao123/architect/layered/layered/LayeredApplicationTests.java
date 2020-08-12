04a906ca15d7ab2b981fe830313d005790556887
周涛 <zhoutao825638@vip.qq.com>
2020-08-12 15:55:05 +0800

    :wrench: update build script

---
+++ layered/src/test/java/com/zhoutao123/architect/layered/layered/LayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoutao123.architect.layered.layered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
